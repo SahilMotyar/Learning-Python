547261bd78f1117b07168f6fd6f246b3287ddaf9
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-14 20:16:40 +0530

    learned indexing and slicing

--- learning_python.py
+++ learning_python.py
@@ -1,17 +1,24 @@
-#learning maths functions:
+''' string slicing: creating substring from another string
+                    we can use 2 methods indexing[] or slice()
+                                         [start:stop:step] '''
 
-import math
+name = "Sahil Motyar"
 
-pi = -3.14
-x = 1
-y = 3
-z = -1
+first_name = name[0:5]
+last_name = name[6:] #we can leave the starting and the ending index, program will assume the values
+funky_name = name[::2] #using the step function, we can leave the starting and ending index if we are using the whole string
+reversed_name = name[::-1] #reversing the string
+print(first_name)
+print(last_name)
+print(funky_name)
+print(reversed_name)
 
-print(round(pi)) #round the number to integer
-print(math.ceil(pi)) #round a numebr to upper integer
-print(math.floor(pi)) #round a number to lower integer
-print(abs(pi)) #gives absolute vale of variable 
-print(pow(pi,3)) #gives power of the variable
-print(math.sqrt(421)) #finds the square root of the value, gives error for negative values
-print(max(x,y,z)) #find the maximum value between the variables 
-print(min(x,y,z)) #find the minimum value between the varibales 
+
+#slicing
+
+website1 = "https://google.com"
+website2 = "https://facebook.com"
+
+slice = slice(8,-4) #using the stop as negative index to take out a specific part of the site, names are of different lengths
+print(website1[slice])
+print(website2[slice])
