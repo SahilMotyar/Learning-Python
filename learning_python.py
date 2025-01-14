8e44aafcf0bb2c3591c89712c8b057e190eb3fcc
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-14 19:47:41 +0530

    learned input method

--- learning_python.py
+++ learning_python.py
@@ -1,3 +1,8 @@
 #learning input method
 name = input("What is your name? ")
+age = int(input("What is your age? "))
+height = float(input("What is your height (in cm): "))
 print(name)
+print("Hello " + name)
+print("Your age is: " + str(age)) #print only takes string values so we use typecasting to make the other methods to string.
+print("You are "+ str(height) + "cm tall")
