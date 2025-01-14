066bc88fddcbab8eb74d48c94a3d081dd817098f
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-14 19:58:31 +0530

    learned maths functions

--- learning_python.py
+++ learning_python.py
@@ -1,8 +1,17 @@
-#learning input method
-name = input("What is your name? ")
-age = int(input("What is your age? "))
-height = float(input("What is your height (in cm): "))
-print(name)
-print("Hello " + name)
-print("Your age is: " + str(age)) #print only takes string values so we use typecasting to make the other methods to string.
-print("You are "+ str(height) + "cm tall")
+#learning maths functions:
+
+import math
+
+pi = -3.14
+x = 1
+y = 3
+z = -1
+
+print(round(pi)) #round the number to integer
+print(math.ceil(pi)) #round a numebr to upper integer
+print(math.floor(pi)) #round a number to lower integer
+print(abs(pi)) #gives absolute vale of variable 
+print(pow(pi,3)) #gives power of the variable
+print(math.sqrt(421)) #finds the square root of the value, gives error for negative values
+print(max(x,y,z)) #find the maximum value between the variables 
+print(min(x,y,z)) #find the minimum value between the varibales 
