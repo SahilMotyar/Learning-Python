cd1ac97c3786e38eb0bfcefec4c143f82998d9d1
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-18 19:20:53 +0530

    learned the input and f string method using madlibs game

--- madlibs game.py
+++ madlibs game.py
@@ -5,3 +5,13 @@ print(f"You are {age} years old.")
 
 # madlibs = word game where you create a story by filling in blacks with random words
 
+adj1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adj2 = input("Enter and adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adj3 = input("Enter and adjective (description): ")
+
+print(f"I was at a {adj1} restaruent today.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adj2} and {verb1}")
+print(f"I was {adj3}")
