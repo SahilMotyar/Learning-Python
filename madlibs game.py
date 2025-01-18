c78d597e6e4106123f8c2784b8df90fdc013c49a
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-18 19:13:40 +0530

    understanding f string and madlibs game

---
+++ madlibs game.py
@@ -0,0 +1,7 @@
+# using f string (it is used in print statement for easy adding of variables using the curly braces )
+
+age = int(input("What is your age? "))
+print(f"You are {age} years old.") 
+
+# madlibs = word game where you create a story by filling in blacks with random words
+
