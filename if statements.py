8d333f7ffe6b638622d46c05b04d3dc104ed282f
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-18 20:17:45 +0530

    used boolean

--- if statements.py
+++ if statements.py
@@ -1,5 +1,5 @@
 # if = do some code if a given condition is true else we do something else
-'''
+
 age = int(input("What is your age? "))
 
 if age >= 100:
@@ -9,7 +9,9 @@ elif age >= 18:
 elif age < 0:
     print("You are not born yet!!")
 else:
-    print("You must be 18+ to sign up.") '''
+    print("You must be 18+ to sign up.") 
+
+#asking user their name and checking if they entered or not    
 
 name = input("Enter your name: ")
 
@@ -17,3 +19,13 @@ if name == "":
     print("You did not enter your name!")
 else:
     print(f"Your name is: {name}")
+
+
+#using boolean values
+
+online = False
+
+if online:
+    print("User is online.")
+else:
+    print("User is NOT online.")
