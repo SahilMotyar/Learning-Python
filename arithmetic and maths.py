b06a169cdc7907eaefb2693b9a0a246837ff65bd
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-18 19:30:11 +0530

    learned basic arithmetic operation

---
+++ arithmetic and maths.py
@@ -0,0 +1,15 @@
+friends = 10
+
+friends += 1 #similar to friends = friends + 1
+print(f"{friends}")
+friends -= 2 #similar to friends = friends - 2
+print(f"{friends}")
+friends *= 3 #similar to friends = friends * 3
+print(f"{friends}")
+friends /= 2 #similar to friends = friends / 2
+print(f"{friends}")
+friends **= 2 # exponential 
+print(f"{friends}")
+remainder = friends % 3  # % finds the remainder of the divison
+
+print(f"{remainder}")
