bd5d05a2b337876a1a7d8c13f4e99401b13f8cbf
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-19 19:11:38 +0530

    updated so that if user enter incorrect operator we have an else statement

--- calculator.py
+++ calculator.py
@@ -6,25 +6,28 @@ num2 = float(input("Enter the second number: "))
 
 if operator == "+":
     add = num1 + num2
-    print(f"The sum of your numbers is: {add}")
+    print(f"The sum of your numbers is: {round(add,3)}")
 
 elif operator == "-":
     sub = num1 - num2
-    print(f"The subtraction of your numbers is: {sub}")
+    print(f"The subtraction of your numbers is: {round(sub,3)}")
 
 elif operator == "*":
     mult = num1*num2
-    print(f"The multiplication of your numbers is: {mult}")
+    print(f"The multiplication of your numbers is: {round(mult,3)}")
 
 elif operator == "/":
     div = num1/num2
-    print(f"The division of your numbers is: {div}")
+    print(f"The division of your numbers is: {round(div,3)}")
 
 elif operator == "**":
     pow = num1**num2
-    print(f"The power of your numbers is: {pow}")
+    print(f"The power of your numbers is: {round(pow,3)}")
+
+elif operator == "%":
+    rem = num1%num2
+    print(f"The remainder of your divison is: {round(rem,3)}")
 
 else:
-    rem = num1%num2
-    print(f"The remainder of your divison is: {rem}")
+    print(f"{operator} is not a valid operator!!")
 
