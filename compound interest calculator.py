bd9ced5f1de4b1abc1e7ac34a2e7c6ceea87e010
Sahil Motyar <motyarsahil1234@gmail.com>
2025-01-22 21:59:37 +0530

    used while True, to use 0 values for the parameters

--- compound interest calculator.py
+++ compound interest calculator.py
@@ -4,26 +4,32 @@ principle = 0
 rate = 0
 time = 0
 
-while principle <= 0:
+while True:
     principle = float(input("Enter the principle amount: "))
-    if principle<=0:
-        print("principle cannot be less than or equals to zero")
+    if principle<0:
+        print("principle cannot be less than zero")
+    else:
+        break
 
-print(principle)
+# print(principle)
 
-while rate <= 0:
+while True:
     rate = float(input("Enter the Interest rate: "))
-    if rate<=0:
-        print("Interest rate cannot be less than or equals to zero")
+    if rate<0:
+        print("Interest rate cannot be less than zero")
+    else:
+        break
 
-print(rate)
+# print(rate)
 
-while time <= 0:
+while True:
     time = int(input("Enter the Time period (in yrs): "))
-    if time<=0:
-        print("Time period cannot be less than or equals to zero")
+    if time<0:
+        print("Time period cannot be less than zero")
+    else:
+        break
 
-print(time)
+# print(time)
 
 total = principle*pow((1 +rate/100),time)
 print(f"Balance after {time} year/s: ${total:.2f}")
